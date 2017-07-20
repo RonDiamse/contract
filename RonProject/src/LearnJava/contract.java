b1fca1e0676493ff65a709ffc55a402874cb9724
Ronaldo Diamse <Ronaldo Diamse@ISD-PF01C3HH>
2017-07-20 01:23:59 -0400

    test upload from eclipse to github

---
+++ RonProject/src/LearnJava/contract.java
@@ -0,0 +1,34 @@
+package LearnJava;
+
+import java.util.Scanner;
+public class contract {
+		
+	public static void main(String[] args) {
+		boolean yn = true;
+		String answer;
+		Scanner input = new Scanner(System.in);
+				
+		System.out.println("Is the contract signed? (yes/no)");
+		answer = input.nextLine();
+		
+		if (answer.equals("yes")) {
+			yn = true;
+		} else if (answer.equals("no")) {
+			yn = false;
+		}
+		
+		
+	if (yn == true) {
+			System.out.println("really? it's " + answer + ". Congratulation! this is what you're waiting for");
+		}
+		else {
+			System.out.println("oh no! get your ass working and get a new job!");
+		}
+		
+		}		
+	}
+
+	
+	
+
+
